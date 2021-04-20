9f6d427b80ea5842b201997f1ca7d0243d27c841
rashik07 <saif67090707@gmail.com>
2021-04-20 14:27:23 -0700

    upload

--- container/sidebar/sidebar.js
+++ container/sidebar/sidebar.js
@@ -6,58 +6,62 @@ const sidebar = () => {
       className="d-flex flex-column p-3 text-white bg-dark"
       style={{  minHeight: "100%" }}
     >
-      <a
-        href="/"
-        className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none  border-bottom"
-      >
-        <span className="fs-4">My Profile</span>
-      </a>
-      <hr />
-      <ul className="nav nav-pills flex-column mb-auto">
-        <li className="nav-item">
-          <a href="../../Profile/Profile_info" className="nav-link active">
-            <i className="fas fa-users" />
-            Personal Info
+       
+
+      
+          <a
+            href="/"
+            className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none  border-bottom"
+          >
+            <span className="fs-4">My Profile</span>
           </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <i className="fas fa-tools" />
-            Career &amp; Application
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <i className="fas fa-university" />
-            Education
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <i className="fas fa-laptop-house" />
-            Employment
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <i className="fas fa-border-none" />
-            Portfolio
-          </a>
-        </li>
-        <a
-          href="/"
-          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none  border-bottom"
-        >
-          <span className="fs-4">My Profile View</span>
-        </a>
-      <hr/>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <i className="fas fa-cogs" />
-            Settings
-          </a>
-        </li>
-      </ul>
+          <hr />
+          <ul className="nav nav-pills flex-column mb-auto">
+            <li className="nav-item">
+              <a href="../../Profile/Profile_info" className="nav-link active">
+                <i className="fas fa-users" />
+                Personal Info
+              </a>
+            </li>
+            <li>
+              <a href="../../Profile/Career_application" className="nav-link text-white">
+                <i className="fas fa-tools" />
+                Career &amp; Application
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link text-white">
+                <i className="fas fa-university" />
+                Education
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link text-white">
+                <i className="fas fa-laptop-house" />
+                Employment
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link text-white">
+                <i className="fas fa-border-none" />
+                Portfolio
+              </a>
+            </li>
+            <a
+              href="/"
+              className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none  border-bottom"
+            >
+              <span className="fs-4">My Profile View</span>
+            </a>
+          <hr/>
+            <li>
+              <a href="#" className="nav-link text-white">
+                <i className="fas fa-cogs" />
+                Settings
+              </a>
+            </li>
+          </ul>
+      
     </div>
   );
 };
