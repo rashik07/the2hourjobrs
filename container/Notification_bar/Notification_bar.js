570b6d7cf9d5ed0a74e6ec6c3d2f14f0b7c2381d
rashik07 <saif67090707@gmail.com>
2021-10-14 18:57:17 -0700

    home page

---
+++ container/Notification_bar/Notification_bar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Notification_bar = () => {
+  return (
+    <div>
+      <div class="hwrap">
+        <div class="hmove">
+          <div class="hitem">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </div>
+          <div class="hitem">Aliquam consequat varius consequat.</div>
+          <div class="hitem">
+            Fusce dapibus turpis vel nisi malesuada sollicitudin.
+          </div>
+          <div class="hitem">Pellentesque auctor molestie orci ut blandit.</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Notification_bar;
