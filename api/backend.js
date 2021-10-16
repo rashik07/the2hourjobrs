a9b0972a190c2ede4a4787f87e72c1a507a63afd
Abdullah Al Rifat <abdullahalrifat95@gmail.com>
2021-10-16 16:34:30 +0600

    api backend updated

--- api/backend.js
+++ api/backend.js
@@ -3,8 +3,10 @@
 import axios from "axios";
 
 let baseUrl;
-
-if (window.location.origin === "http://localhost:3000") {
+if (typeof window == "undefined") {
+  baseUrl = "http://127.0.0.1:8000/api/";
+}
+else if (window.location.origin === "http://localhost:3000") {
   baseUrl = "http://127.0.0.1:8000/api/";
 } else {
   baseUrl = window.location.origin + "/api/";
