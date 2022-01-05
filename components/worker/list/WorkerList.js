2e38fae48b93e233620aaf0cb07b32ac98b4d2ee
rashik07 <saif67090707@gmail.com>
2022-01-05 17:53:45 +0600

    update worker list

--- components/worker/list/WorkerList.js
+++ components/worker/list/WorkerList.js
@@ -12,6 +12,7 @@ const JobList = ({
   all_workers,
   filtered_workers,
   showFilterWorker,
+  isSignedIn,
 }) => {
   const [reaload, setReload] = useState(false);
 
@@ -21,39 +22,48 @@ const JobList = ({
   }, [reaload]);
 
   console.log(all_workers);
+ 
+    if (showFilterWorker) {
+      return filtered_workers.map((worker) => {
+        if (worker.available_for_work == true) {
+          return (
+            <WorkerItem key={worker.id} worker={worker} setReload={setReload} />
+          );
+        }
+      });
+    }
+    if (!isSignedIn) {
+      return (
+        <h1 style={{ color: "#AEB6BF", marginTop: "20%", marginLeft: "20%" }}>
+          Please login to access this feature
+        </h1>
+      );
+     } else {
+    if (all_workers) {
+      return all_workers.map((worker) => {
+        if (worker.available_for_work == true) {
+          // console.log(worker.available_for_work);
+          return (
+            <WorkerItem key={worker.id} worker={worker} setReload={setReload} />
+          );
+        }
+        //  return <WorkerItem key={worker.id} worker={worker} />;
+      });
+    }
+    // else{
+    //   return "please login";
+    // }
+  }
+    return null;
   
-  if (showFilterWorker) {
-    return filtered_workers.map((worker) => {
-      if (worker.available_for_work == true) {
-        return (
-          <WorkerItem key={worker.id} worker={worker} setReload={setReload} />
-        );
-      }
-    });
-  }
-
-  if (all_workers ){
-    return  all_workers.map((worker) => {
-      if (worker.available_for_work == true) {
-        // console.log(worker.available_for_work);
-        return (
-          <WorkerItem key={worker.id} worker={worker} setReload={setReload} />
-        );
-      }
-      //  return <WorkerItem key={worker.id} worker={worker} />;
-    });
-  }
-  // else{
-  //   return "please login";
-  // }
-
-  return null;
+  
 };
 
 const mapStateToProps = (state) => {
   return {
     all_workers: Object.values(state.user.all_workers),
     filtered_workers: state.user.filtered_workers,
+    isSignedIn: state.auth.isSignedIn,
   };
 };
 
